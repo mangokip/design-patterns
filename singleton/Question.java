008ba7069656bb1b9a31a56c348e4c7fe10a6ede
Bryce Klein <bryceian2003@gmail.com>
2024-09-22 17:56:52 -0400

    updated files

--- singleton/Question.java
+++ singleton/Question.java
@@ -1,3 +1,8 @@
+/**
+ * This class creates a Question utilizing the questions.txt file pulled from the DataLoader.
+ *
+ * @author Bryce Klein
+ */
 package singleton;
 
 import java.util.ArrayList;
@@ -9,6 +14,14 @@ public class Question {
     private ArrayList<String> answers;
     private int correctAnswer;
 
+    /**
+     * This is a basic constructor, with answers being converted into an
+     * ArrayList for dynamic editing.
+     *
+     * @param question - question prompt from file
+     * @param correctAnswer - correct answer as an index
+     * @param answers - an array of answers that is converted into an ArrayList
+     */
     public Question(String question, int correctAnswer, String[] answers) {
         this.question = question;
         this.correctAnswer = correctAnswer;
@@ -19,8 +32,14 @@ public class Question {
         return question + "\n" + "1. " + answers.get(0) + "\n" + "2. " + answers.get(1) + "\n" + "3. " + answers.get(2) + "\n" + "4. " + answers.get(3);
     }
 
+    /**
+     * Basic boolean check on correctness of answer
+     *
+     * @param userAnswer - users answer which should be a standard int
+     * @return
+     */
     public boolean isCorrect(int userAnswer) {
-        return userAnswer == correctAnswer + 1;
+        return userAnswer == correctAnswer + 1; //+1 to ensure user answer is based off display and not actual indices
     }
 
     public String getCorrectAnswer() {
--- singleton/TriviaGame.java
+++ singleton/TriviaGame.java
@@ -1,3 +1,8 @@
+/**
+ * This class creates a single TriviaGame instance to be used as the base game.
+ *
+ * @author Bryce Klein
+ */
 package singleton;
 
 import java.util.ArrayList;
@@ -11,11 +16,22 @@ public class TriviaGame {
     private Random rand;
     private ArrayList<Question> questions;
 
+    /**
+     * Private constructor to be utilized with getInstance. The questions are
+     * loaded from the file, and a random number object is created to randomize
+     * questions.
+     */
     private TriviaGame() {
         this.questions = DataLoader.getTriviaQuestions();
         this.rand = new Random();
     }
 
+    /**
+     * This grabs the single instance of Trivia Game so that it can be
+     * interacted with
+     *
+     * @return TriviaGame object that is grabbed, and is created if null
+     */
     public static TriviaGame getInstance() {
         if (triviaGame == null) {
             triviaGame = new TriviaGame();
@@ -23,37 +39,60 @@ public class TriviaGame {
         return triviaGame;
     }
 
+    /**
+     * A less than standard getter, where a Question object is created by
+     * utilizing a random index value to completely randomize the question pool.
+     *
+     * @return - Question object based off randomQuestion variable.
+     */
     public Question getQuestion() {
         if (questions.isEmpty()) {
             return null;
         }
         int index = rand.nextInt(questions.size());
         Question randomQuestion = questions.get(index);
-        questions.remove(index);
-        return randomQuestion;
 
+        // Remove the question after it's been returned
+        questions.remove(index);
+
+        return randomQuestion;  // Return the question and immediately remove it
     }
 
+    /**
+     * Boolean check to see if ArrayList<Question> questions has more questions
+     * available
+     *
+     * @return - boolean on whether or not the list is empty.
+     */
     public boolean hasMoreQuestions() {
         if (questions.isEmpty()) {
-            System.out.println("--- CRITICAL: OUT OF QUESTIONS! Please Quit now to see your score or forever hold your peace.");
             return false;
 
         }
         return true;
     }
 
+    /**
+     * Checks if questions are still present, then adds to numGames and score
+     */
     public void winRound() {
-        if (hasMoreQuestions()) {  // Only count if there are more questions
-            numGames++;
-            score++;
+        if (!hasMoreQuestions()) {
+            System.out.println("--- CRITICAL: OUT OF QUESTIONS! Please Quit now to see your score or forever hold your peace.");
         }
+        numGames++;
+        score++;
+
     }
 
+    /**
+     * Checks if questions are still present, and then only adds to numGames
+     */
     public void loseRound() {
-        if (hasMoreQuestions()) {  // Only count if there are more questions
-            numGames++;
+        if (!hasMoreQuestions()) {
+            System.out.println("--- CRITICAL: OUT OF QUESTIONS! Please Quit now to see your score or forever hold your peace.");
         }
+        numGames++;
+
     }
 
     public String getScores() {
